f7453d609dba1072258e225b738d2552fafa0ca7
정호경 <luclipse@gmail.com>
2021-04-23 14:18:35 +0900

    초기소스

---
+++ src/main/java/egovframework/example/config/ContextTransaction.java
@@ -0,0 +1,22 @@
+package egovframework.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@ImportResource("classpath:/egovframework/spring/context-transaction.xml")
+public class ContextTransaction {
+
+    @Bean
+    public DataSourceTransactionManager txManager(DataSource dataSource){
+        DataSourceTransactionManager dstm = new DataSourceTransactionManager();
+        dstm.setDataSource(dataSource);
+        return dstm;
+    }
+}
---
+++ src/main/java/egovframework/example/config/ServletContext.java
@@ -0,0 +1,4 @@
+package egovframework.example.config;
+
+public class ServletContext {
+}
