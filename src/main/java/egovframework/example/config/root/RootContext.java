2d84f326552375d32ba18fc337da6b2a24c120eb
정호경 <luclipse@gmail.com>
2021-05-12 13:51:51 +0900

    - application.properties 설정 추가

--- src/main/java/egovframework/example/config/root/RootContext.java
+++ src/main/java/egovframework/example/config/root/RootContext.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({ContextAspect.class, ContextCommon.class, ContextDataSource.class
         , ContextIdgen.class, ContextMapper.class, ContextSqlMap.class, ContextProperties.class
-        , ContextTransaction.class, ContextValidator.class
+        , ContextTransaction.class, ContextValidator.class, ContextConfig.class
 })
 public class RootContext {
 }
