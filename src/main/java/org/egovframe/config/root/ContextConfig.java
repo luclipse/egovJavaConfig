6dcc3b312bd23234bc7e821b90636c9cf343159a
luclipse <luclipse@gmail.com>
2023-04-06 19:05:27 +0900

    application.properties 경로 변경

--- src/main/java/org/egovframe/config/root/ContextConfig.java
+++ src/main/java/org/egovframe/config/root/ContextConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:egovframework/config/application.properties")
+@PropertySource("classpath:/application.properties")
 public class ContextConfig {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfig() {
